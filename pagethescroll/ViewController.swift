8b837e2c051c17c9a4561f0fd5d58e869241bc40
amir masoud <am.mohtaji@hotmail.com>
2017-06-24 22:35:59 +0430

    2nd level design first code

--- pagethescroll/ViewController.swift
+++ pagethescroll/ViewController.swift
@@ -11,16 +11,26 @@ import UIKit
 class ViewController: UIViewController {
 var images = [UIImageView] ()
     
+    @IBOutlet weak var scrollview: UIScrollView!
+   // var contentwidth : CGFloat = 0.0
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        var heigth : CGFloat = 0.0
+        var width : CGFloat = 0.0
+        width = 150
+        heigth = 150
         for x in 0...2 {
-           let image = UIImage(named: "icon\(x).png")
-            images.append(UIImageView(image: image))
-            
+            let image = UIImage(named: "icon\(x).png")
+            let imageview = UIImageView(image: image)
+            images.append(imageview)
+          //  var newx : CGFloat = 0.0
+            //newx = view.frame.size.height * CGFloat(x) + view.frame.midX/2
+            scrollview.addSubview(imageview)
+            imageview.frame = CGRect(x:(view.frame.size.width/2) - width/2 , y:(view.frame.height/2) - heigth/2 , width:width, height:heigth)
+            //contentwidth += view.frame.size.width * CGFloat(x) + view.frame.midX/2
         }
-        
-        
+        //scrollview.contentSize = CGSize(width: contentwidth, height: view.frame.size.height)
         
     }
 
